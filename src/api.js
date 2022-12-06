fd2de11a089ef0a4c6cb1e73bf842a417731ae43
robbie <robbie_mcdonough@hotmail.com>
2022-12-06 15:13:19 +0000

    added loading state

--- src/api.js
+++ src/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-const gamesApi = axios.create({
+export const gamesApi = axios.create({
   baseURL: "https://nice-lime-firefly-hose.cyclic.app/",
 });
 
-export default gamesApi;
+export const getReviews = () => {
+  return gamesApi.get("/api/reviews").then(({ data: { reviews } }) => {
+    return reviews;
+  });
+};
+
+//export get request here
+
+//add loading state
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,33 +1,45 @@
-import { useEffect } from "react";
-import gamesApi from "../api";
+import { useEffect, useState } from "react";
+import { getReviews } from "../api";
 import "../App.css";
 
 function Home({ reviews, setReviews }) {
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    gamesApi
-      .get("api/reviews")
+    setLoading(true);
+    getReviews()
       .then((res) => res)
       .then((results) => {
-        console.log(results.data.reviews);
-        setReviews(results.data.reviews);
+        setReviews(results);
+        console.log(reviews);
+
+        setLoading(false);
       });
   }, []);
   return (
     <div>
-      <ul>
-        {reviews.map((review) => {
-          return (
-            <li key={review.review_id} className="Review-list">
-              <h3>{review.title}</h3>
-              <p>Category: {review.category}</p>
-              <img src={review.review_img_url} alt={review.title} height="100px" />
-              <p>Votes: {review.votes}</p>
-              <p>Comments: {review.comment_count}</p>
-              <button className="Full-review-button">See full review</button>
-            </li>
-          );
-        })}
-      </ul>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {reviews.map((review) => {
+            return (
+              <li key={review.review_id} className="Review-list">
+                <h3>{review.title}</h3>
+                <p>Category: {review.category}</p>
+                <img
+                  src={review.review_img_url}
+                  alt={review.title}
+                  height="100px"
+                />
+                <p>Votes: {review.votes}</p>
+                <p>Comments: {review.comment_count}</p>
+                <button className="Full-review-button">See full review</button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
